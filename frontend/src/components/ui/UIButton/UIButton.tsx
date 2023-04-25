1834a3f7f8bf4233c7dfae6340a25d011bba3e19
slry <v.rybalchenko@innopolis.university>
2023-04-25 03:10:15 +0300

    feat: add UIButton component

---
+++ frontend/src/components/ui/UIButton/UIButton.tsx
@@ -0,0 +1,15 @@
+import classes from './UIButton.module.css'
+
+type UIButtonProps = {
+  text: string
+}
+
+const UIButton: React.FC<UIButtonProps> = ({ text }) => {
+  return (
+    <div className={classes.btn}>
+      <p className={classes.btn_text}>{text}</p>
+    </div>
+  )
+}
+
+export default UIButton
