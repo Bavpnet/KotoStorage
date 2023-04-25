35096629f34a3a214d8c4c9a5940d477ef00417e
slry <v.rybalchenko@innopolis.university>
2023-04-25 23:12:58 +0300

    feat: add responsiveness to Navbar component

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,18 +1,46 @@
+import { useState } from 'react'
+import Hamburger from '../ui/Hamburger/Hamburger'
 import UIButton from '../ui/UIButton/UIButton'
 import classes from './Navbar.module.css'
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <div className={classes.navbar}>
       <p className={classes.logo}>KotoStorage</p>
-      <div className={classes.links}>
-        <p>About</p>
-        <p>Upload</p>
-        <p>Gallery</p>
-      </div>
+      <ul className={`${classes.links} ${isMenuOpen ? classes.active : ''}`}>
+        <li className={classes.line}>
+          <span />
+        </li>
+        <li className={`${classes.links__btn} ${classes.indented}`}>About</li>
+        <li className={`${classes.links__btn} ${classes.indented}`}>Upload</li>
+        <li className={`${classes.links__btn} ${classes.indented}`}>Gallery</li>
+        <li className={classes.line}>
+          <span />
+        </li>
+        <li className={classes.info}>
+          <p className={`${classes.info__title} ${classes.indented}`}>
+            Follow us in the media
+          </p>
+          <p className={`${classes.info__desc} ${classes.indented}`}>
+            Hear about exclusive news and changes in the cats world
+          </p>
+          <ul className={classes.social}>
+            <li>Instagram</li>
+            <li>Telegram</li>
+            <li>Github</li>
+          </ul>
+        </li>
+      </ul>
       <div className={classes.gh_btn}>
         <UIButton text="GitHub" />
       </div>
+      <Hamburger onClick={toggleMenu} />
     </div>
   )
 }
