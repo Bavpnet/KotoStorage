5688438e330911300eb2fc5aa33db5bc2aa193af
slry <v.rybalchenko@innopolis.university>
2023-04-28 16:05:56 +0300

    feat: add ImagePreview 404 image and some style fix

--- frontend/src/components/ImagePreview/ImagePreview.tsx
+++ frontend/src/components/ImagePreview/ImagePreview.tsx
@@ -1,3 +1,4 @@
+import { SyntheticEvent, useEffect, useState } from 'react'
 import styles from './ImagePreview.module.css'
 
 type ImagePreviewProps = {
@@ -5,10 +6,19 @@ type ImagePreviewProps = {
 }
 
 export const ImagePreview: React.FC<ImagePreviewProps> = ({ url }) => {
+  const url404 = 'https://http.cat/404'
+
+  const onError = (e: SyntheticEvent<HTMLImageElement, Event>) => {
+    console.log('error')
+    if (e.currentTarget.src !== url404) {
+      e.currentTarget.src = url404
+    }
+  }
+
   return (
     <picture className={styles.imgContainer}>
       <h3 className={styles.heading3}>Preview of the photo:</h3>
-      {url && <img src={url} alt="cat" className={styles.img} />}
+      <img className={styles.img} src={url} alt="preview" onError={onError} />
     </picture>
   )
 }
