0fceb3a0a5193b214cb8469263abf55ef7e20ba8
slry <v.rybalchenko@innopolis.university>
2023-04-29 17:59:06 +0300

    refactor: extend UIAnchorProps with HTMLAnchorElementProps

--- frontend/src/components/ui/UIAnchor/UIAnchor.tsx
+++ frontend/src/components/ui/UIAnchor/UIAnchor.tsx
@@ -2,12 +2,12 @@ import styles from './UIAnchor.module.css'
 
 type UIAnchorProps = {
   text: string
-  href: string
-  target?: '_blank' | '_top' | '_self' | '_parent'
 }
 
-export const UIAnchor: React.FC<UIAnchorProps> = ({ text, href, target }) => (
-  <a className={styles.anc} href={href} target={target ? target : '_blank'}>
+export const UIAnchor: React.FC<
+  React.HTMLProps<HTMLAnchorElement> & UIAnchorProps
+> = ({ text, ...rest }) => (
+  <a className={styles.anc} {...rest}>
     {text}
   </a>
 )
